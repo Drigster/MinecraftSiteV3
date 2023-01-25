dd37d5b0224c0e73f09654873d002e3a45ff3a4e
Gabriel Jõe <gabriel.j6e@gmail.com>
2023-01-26 01:00:53 +0200

    add styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,9 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./views/*.html"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
