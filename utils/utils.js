6c4b6b1db46e235b9cbe3d036bda1daa1a6d62f3
Gabriel Jõe <gabriel.j6e@gmail.com>
2023-02-14 22:34:16 +0200

    move transport out of sendToken function

--- utils/utils.js
+++ utils/utils.js
@@ -4,6 +4,40 @@ import nodemailer from 'nodemailer';
 
 dotenv.config();
 
+let transporter;
+try {
+    if(process.env.NODE_ENV === "development"){
+        let testAccount = await nodemailer.createTestAccount();
+        transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+              user: testAccount.user,
+              pass: testAccount.pass
+            }
+        });
+    }
+    else{
+        transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: true,
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASSWORD
+            },
+            tls: {
+                rejectUnauthorized: false,
+            }
+        });
+    }
+    console.log("Transport ready");
+} catch (error) {
+    console.log("ERROR\n");
+    console.log(error);
+}
+
 export function createVerificationToken(email) {
     const token = jwt.sign({ email }, process.env.JWT_SECRET, {
         expiresIn: '15m'
@@ -21,41 +55,13 @@ export function verifyVerificationToken(token) {
 }
 
 export async function sendVerificationToken(email) {
-    let transporter;
-    if(process.env.NODE_ENV === "development"){
-        let testAccount = await nodemailer.createTestAccount();
-        transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false,
-            auth: {
-              user: testAccount.user,
-              pass: testAccount.pass
-            }
-        });
-    }
-    else{
-        transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false,
-            auth: {
-              user: process.env.SMTP_USER,
-              pass: process.env.SMTP_PASSWORD
-            }
-        });
-    }
 
     const token = createVerificationToken(email);
 
     let info = await transporter.sendMail({
-        from: '"DicePVP" <auth@dicepvp.ee>',
+        from: '"DicePVP" <auth@disepvp.ee>',
         to: email,
         subject: "Verify your account!",
         html: `<a href='${process.env.BASE_URL}/verify/${token}'>Hello world?</a>`
     });
-
-    if(process.env.NODE_ENV === "development"){
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    }
 }
