5f62d0dc507739b60b4e0a6a2d75e8917f0ce3db
Gabriel Joe <gabriel.j6e@gmail.com>
2023-05-11 18:52:15 +0300

    fix error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,10 @@ import { Logger } from "./../utils/logger.js";
 
 const logger = new Logger();
 
-export default (err, req, res) => {
-	logger.log("ERROR", err.stack);
-	res.render("error", {status: 500, message: "Что-то сломалось :)"});
+export default (err, req, res, next) => {
+	logger.log("ERROR", err);
+	if(process.env.NODE_ENV === "development"){
+		return next(err);
+	}
+	return res.render("error", { error: err });
 };
